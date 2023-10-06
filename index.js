c04a1a71aa825f5314f096cd8c368c8fc2e96ed7
Lueder Budde <lueder.budde@gmail.com>
2023-10-06 15:32:52 +0200

    bookmarkButton and answer button with wrong but working solution

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // phew… not a lot going on here. Please add some code!
+const bookmarkButton = document.querySelector(
+  '[data-js="card__button-bookmark"]'
+);
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+  console.log(bookmarkButton);
+});
+
+// Answer Button
+
+const cardButtonAnswer = document.querySelector(
+  '[data-js="card__button-answer"]'
+);
+const cardAnswer = document.querySelector('[data-js="card__answer"]');
+
+cardButtonAnswer.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer--active");
+  console.log(cardButtonAnswer);
+});
